e05ae0593cb78dd336c7b8d652428c34161078d5
Damian Hickey <dhickey@gmail.com>
2014-05-08 18:35:35 +0200

    Unused using directive.

--- src/BusLite/InMemoryNamespace.cs
+++ src/BusLite/InMemoryNamespace.cs
@@ -4,7 +4,6 @@
     using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Security.Cryptography;
     using System.Threading.Tasks;
     using Microsoft.ServiceBus.Messaging;
 
